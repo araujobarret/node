5b8257f461c5944666d33af89a9980f5d56e9370
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-08-09 19:59:50 -0300

    Adding note working

--- notes-node/playground/json.js
+++ notes-node/playground/json.js
@@ -6,7 +6,23 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 
-let personStr = '{"name": "Carlos", "age": 28}';
-let objJson = JSON.parse(personStr)
-console.log(typeof objJson);
-console.log(objJson);
+// let personStr = '{"name": "Carlos", "age": 28}';
+// let objJson = JSON.parse(personStr)
+// console.log(typeof objJson);
+// console.log(objJson);
+
+const fs = require('fs');
+
+let originalNote = {
+	title: 'Some title',
+	body: 'Some body'
+};
+
+let originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+let note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
