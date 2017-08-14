bd51d890390466831e7fd5c09a20494de56477bc
Carlos A A Barreto <araujobarret@gmail.com>
2017-08-14 00:56:52 -0300

    Weather api working

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,26 +1,44 @@
+// const request = require('request');
+// const yargs = require('yargs');
+
+// const geocode = require('./geocode/geocode.js')
+
+// const argv = yargs
+// 	.options({
+// 		a: {
+// 			demand: true,
+// 			alias: 'address',
+// 			describe: 'Address to fetch weather for',
+// 			string: true
+// 		}
+// 	})
+// 	.help()
+// 	.alias('Help', 'h')
+// 	.argv;
+
+// geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+// 	if(errorMessage)
+// 		console.log(errorMessage);
+// 	else{
+// 		console.log(JSON.stringify(results, undefined, 2));
+// 	}
+
+// });
+
+//ff16edc03ca403a096295e18807565a1
+
 const request = require('request');
-const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js')
-
-const argv = yargs
-	.options({
-		a: {
-			demand: true,
-			alias: 'address',
-			describe: 'Address to fetch weather for',
-			string: true
-		}
-	})
-	.help()
-	.alias('Help', 'h')
-	.argv;
-
-geocode.geocodeAddress(argv.a, (errorMessage, results) => {
-	if(errorMessage)
-		console.log(errorMessage);
-	else{
-		console.log(JSON.stringify(results, undefined, 2));
-	}
-
+request({
+	url: 'https://api.forecast.io/forecast/ff16edc03ca403a096295e18807565a1AAAA/-22.932,-43.231',
+	json: true
+}, (error, response, body) => {
+	if(error)
+		console.log('No response from the forecast.io server');
+	else		
+		if(response.statusCode === 400)
+			console.log('Unable to fetch weather');
+		else
+			if(response.statusCode === 200)
+				console.log(body.currently.temperature);
 });
