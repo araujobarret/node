93a0cda034f6107cada10e7f6d9404943185c146
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-08-16 17:01:35 -0300

    Middleware with express

--- node-server/server.js
+++ node-server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err)
+			console.log('Unable to append to the file');
+	});
+
+	next();
+});
 
 hbs.registerHelper('getCurrentYear', () =>{
 	return new Date().getFullYear();
@@ -16,7 +30,7 @@ hbs.registerHelper('screamIt', (text) =>{
 });
 
 app.get('/', (req, res) => {
-	res.render('Home.hbs', {
+	res.render('home.hbs', {
 		title: 'Home Page',		
 		message: 'Hbs templating'
 	});
