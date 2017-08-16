81545169691c8265228a523f2e8e9e06124e4c47
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-08-16 17:17:49 -0300

    More middleware usage

--- node-server/server.js
+++ node-server/server.js
@@ -6,7 +6,6 @@ let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
 	let now = new Date().toString();
@@ -20,6 +19,13 @@ app.use((req, res, next) => {
 
 	next();
 });
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerHelper('getCurrentYear', () =>{
 	return new Date().getFullYear();
